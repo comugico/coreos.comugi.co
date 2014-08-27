4a95557eb6f4debbce6d4c80110ebf68c4bcb5f5
Shuma Yoshioka <s64.stdio@gmail.com>
2014-08-27 10:18:08 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 
 pushd ./aipo
 docker build --tag="aipo" .
-docker -d --name="aipo" aipo
+docker run -d --name="aipo" aipo
 sudo cp ./services/aipo.service /etc/systemd/system/
 sudo systemctl enable aipo.service
 sudo systemctl restart aipo.service
@@ -11,7 +11,7 @@ popd
 
 pushd ./aipo2
 docker build --tag="aipo2" .
-docker -d --name="aipo2" aipo2
+docker run -d --name="aipo2" aipo2
 sudo cp ./services/aipo2.service /etc/systemd/system/
 sudo systemctl enable aipo2.service
 sudo systemctl restart aipo2.service
@@ -19,7 +19,7 @@ popd
 
 pushd ./reverseproxy
 docker build --tag="reverseproxy" .
-docker -d --name="reverseproxy" --link aipo:aipo --link aipo2:aipo2 -p 80:80 reverseproxy
+docker run -d --name="reverseproxy" --link aipo:aipo --link aipo2:aipo2 -p 80:80 reverseproxy
 sudo cp ./services/reverseproxy.service /etc/systemd/system/
 sudo systemctl enable reverseproxy.service
 sudo systemctl restart reverseproxy.service
